9057bbc87bfc26e26c9ba7314e12cf43f9543a7e
DOoOoOoOnut <intro2eun@gmail.com>
2024-02-14 05:59:00 +0900

    search.scss 80% 완성

--- script/join_phone.js
+++ script/join_phone.js
@@ -5,7 +5,8 @@ const next_btn = document.querySelector('#next_btn');
 const error = document.querySelectorAll('.error');
 const send_container = document.querySelector('.after');
 const notice = document.querySelector('.title p')
-console.log(user_num,error);
+const reset_btn = document.querySelectorAll('.reset');
+console.log(user_num,error,reset_btn);
 let back 
 
 // 입력창
@@ -33,7 +34,6 @@ for(let i of user_num){
     i.addEventListener('input',(e)=>{
         e.target.value = e.target.value.replace(/\D/g, '')
     })
-    
 }
 
 // 전화번호 형식만 입력가능
@@ -56,6 +56,7 @@ user_num[0].addEventListener('input',(e)=>{
         error[0].innerText = '휴대폰 번호를 확인해주세요.'
         user_num[0].style.border = '1px solid #f45858'
     }
+    
 })
 
 user_num[1].addEventListener('input',(j)=>{
@@ -67,4 +68,12 @@ user_num[1].addEventListener('input',(j)=>{
         next_btn.style.cursor = 'pointer'
         error[0].innerText = ''
     }
+})
+
+// x 버튼 누르면 작성 문구 초기화
+reset_btn.forEach((t,i)=>{
+    t.style.display = 'none'
+    t.addEventListener('click',()=>{
+        user_num[i].value = ''
+    })
 })
