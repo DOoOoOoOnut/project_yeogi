7dc1ad63add84aeddbb590cf26c05b33f870e7c5
Ahn il hyuk <ilhyuk0504@gmail.com>
2024-02-20 18:24:45 +0900

    지도미완성

---
+++ script/detail.js
@@ -0,0 +1,11 @@
+// 첫 번째 지도 초기화
+var map1 = new naver.maps.Map('map1', {
+    center: new naver.maps.LatLng(37.5665, 126.9780),
+    zoom: 10
+});
+
+// 두 번째 지도 초기화
+var map2 = new naver.maps.Map('map2', {
+    center: new naver.maps.LatLng(37.4824, 127.0390),
+    zoom: 12
+});
