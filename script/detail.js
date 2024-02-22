aeeb0987d7f301ab23b43da65c15d5abe80b4d6a
Ahn il hyuk <ilhyuk0504@gmail.com>
2024-02-22 18:14:30 +0900

    상페 반응형

--- script/detail.js
+++ script/detail.js
@@ -48,12 +48,12 @@ let swiper = new Swiper('.swiper-container', {
 
 let swiper02 = new Swiper('#thum_review_contents', {
      // 한 번에 보여줄 슬라이드 수
-    slidesPerView: 2,
-    // 슬라이드 사이의 간격
-    spaceBetween: 10,
-    // 한 번에 넘길 슬라이드 수
-    slidesPerGroup: 1,
-    centeredSlides : true,
+    // slidesPerView: 2,
+    // // 슬라이드 사이의 간격
+    // spaceBetween: 10,
+    // // 한 번에 넘길 슬라이드 수
+    // slidesPerGroup: 1,
+    // centeredSlides : true,
     navigation: {
         nextEl: ".swiper-button-next02",
         prevEl: ".swiper-button-prev02",
@@ -61,14 +61,14 @@ let swiper02 = new Swiper('#thum_review_contents', {
     breakpoints: {
          // 윈도우 화면 너비가 800 이상일 때(min-width 기준)
         900: {
-        slidesPerView: 1,
+        slidesPerView: 1.5,
         spaceBetween: 10,
         slidesPerGroup: 1,
         centeredSlides : false,
     },
     // 화면 너비가 1200px 이상일 때
     1200: {
-        slidesPerView: 1,
+        slidesPerView: 1.5,
         spaceBetween: 25,
         slidesPerGroup: 1,
         centeredSlides : false,
@@ -78,28 +78,59 @@ let swiper02 = new Swiper('#thum_review_contents', {
 
 let swiper03 = new Swiper('#thum_photo', {
      // 한 번에 보여줄 슬라이드 수
-    slidesPerView: 6,
+    slidesPerView: 5.5,
     // 슬라이드 사이의 간격
     spaceBetween: 10,
     // 한 번에 넘길 슬라이드 수
-    slidesPerGroup: 6,
+    slidesPerGroup: 1,
     centeredSlides : true,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next03",
+        prevEl: ".swiper-button-prev03",
     },
     breakpoints: {
          // 윈도우 화면 너비가 800 이상일 때(min-width 기준)
         900: {
-        slidesPerView: 1,
+        slidesPerView: 5.5,
         spaceBetween: 10,
         slidesPerGroup: 1,
         centeredSlides : false,
     },
     // 화면 너비가 1200px 이상일 때
     1200: {
-        slidesPerView: 1,
+        slidesPerView: 5.5,
         spaceBetween: 25,
+        slidesPerGroup: 1,
+        centeredSlides : false,
+    }
+},
+})
+
+
+let swiper04 = new Swiper('#room_type', {
+    // 한 번에 보여줄 슬라이드 수
+    slidesPerView: 4,
+   // 슬라이드 사이의 간격
+    spaceBetween: 10,
+   // 한 번에 넘길 슬라이드 수
+    slidesPerGroup: 1,
+    centeredSlides : true,
+    navigation: {
+        nextEl: ".swiper-button-next04",
+        prevEl: ".swiper-button-prev04",
+    },
+    breakpoints: {
+        // 윈도우 화면 너비가 800 이상일 때(min-width 기준)
+        900: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+        centeredSlides : false,
+    },
+   // 화면 너비가 1200px 이상일 때
+    1200: {
+        slidesPerView: 4,
+        spaceBetween: 10,
         slidesPerGroup: 1,
         centeredSlides : false,
     }
