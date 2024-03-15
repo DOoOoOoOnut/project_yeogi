b765c170e7a62c4513714ff18b1a40603b2a1941
moooomina <viva3927@google.com>
2024-03-15 17:50:18 +0900

    main js 90%

--- script/main.js
+++ script/main.js
@@ -31,8 +31,26 @@ const total_night = document.querySelectorAll('.total_night')
 const date_reset = document.querySelectorAll('.date_reset span')
 const date_reset_tab = document.querySelector('.date_reset_tab')
 let hover = true
-// console.log(userSearch,searchInput,searchRanking)
-console.log(date_reset)
+// 객실 인원 증가 감소 
+const num_minus_room = document.querySelectorAll('.user_room_select .num_minus')
+const num_minus_adult = document.querySelectorAll('.user_adult_select .num_minus')
+const num_minus_child = document.querySelectorAll('.user_children_select .num_minus')
+const num_plus_room = document.querySelectorAll('.user_room_select .num_plus')
+const num_plus_adult = document.querySelectorAll('.user_adult_select .num_plus')
+const num_plus_child = document.querySelectorAll('.user_children_select .num_plus')
+const children_notice = document.querySelectorAll('.children_notice')
+const children_age = document.querySelectorAll('.children_age')
+const down = document.querySelectorAll('i[class*=icon]')
+const adult_num = document.querySelectorAll('.adult_num')
+const room_num = document.querySelectorAll('.room_num')
+const children_num = document.querySelectorAll('.children_num')
+const user_children_age = document.querySelectorAll('.user_children_age')
+const children_age_select = document.querySelectorAll('.children_age_select')
+// const children_age_select_span = document.querySelectorAll('.children_age_select span')
+const select_view = document.querySelectorAll('.select_view')
+let room_count = 1
+let adult_count = 2
+let child_count = 0
 
 
 //?
@@ -72,9 +90,8 @@ function resetCount() {
     count = 0; // count를 0으로 초기화
 }
 
-let count = 0 //0 == 날짜 선택안했을 떄(초기값) //1 == 시작날짜를 선택했을 때 //2 == 종료날짜를 선택했을때(팝업닫힘)
+let count = 0 //0 == 날짜 선택안했을 때(초기값) //1 == 시작날짜를 선택했을 때 //2 == 종료날짜를 선택했을때(팝업닫힘)
 //클릭한 td가 몇일인지??
-// console.log('=============')
 // console.log(t.parentElement.parentElement.parentElement.title)
 // console.log(`0${t.parentElement.parentElement.parentElement.title}.${t.innerText} ${t.title}`)
 dateSelectTd.forEach((t) => {
@@ -104,6 +121,7 @@ dateSelectTd.forEach((t) => {
                 resetCount()
                 date_select_lastday[0].innerHTML = data_select
                 date_select_lastday[1].innerHTML = data_select
+                userSelect.style.display = 'block'
             }
         });
     }
@@ -139,6 +157,119 @@ dateSelectBox.addEventListener('click', function(event) {
 searchRanking.addEventListener('click', function(event) {
     event.stopPropagation();
 });
+
+// 3.인원수 체크
+// 인원수 표시
+// for
+//아이 나이 가리기
+children_notice[1].style.display = 'none'
+children_age[1].style.display = 'none'
+children_age[2].style.display = 'none'
+console.log(children_age)
+//성인, 아이, 객실 증가용 
+num_plus_adult.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        adult_count++
+        this.previousElementSibling.innerHTML = adult_count
+        adult_num[1].innerHTML = adult_count
+    })
+})
+num_plus_child.forEach(function(t, i) {
+    t.addEventListener('click', function() {
+        if (child_count >= 2) {
+            alert('최대 2명까지 예약 가능합니다.');
+        } else {
+            child_count++;
+            this.previousElementSibling.innerHTML = child_count;
+            children_notice[1].style.display = 'block';
+            children_num[1].innerHTML = child_count;
+            if (child_count == 1) {
+                children_age[1].style.display = 'flex';
+                children_age[2].style.display = 'none';
+            } else if (child_count == 2) {
+                children_age[2].style.display = 'flex';
+            }
+        }
+    });
+});
+num_plus_room.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        room_count++
+        this.previousElementSibling.innerHTML = room_count
+        room_num[1].innerHTML = room_count
+
+    })
+})
+//성인, 아이, 객실 감소용
+num_minus_adult.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        if(adult_count > 1){
+            adult_count--
+            this.nextElementSibling.innerHTML = adult_count
+            adult_num[1].innerHTML = adult_count
+        }
+    })
+})
+num_minus_child.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        if(child_count > 0){
+            child_count--
+            this.nextElementSibling.innerHTML = child_count
+            children_num[1].innerHTML = child_count
+        }
+        if(child_count == 0){
+            children_notice[1].style.display = 'none'
+            children_age[1].style.display = 'none'
+            children_age[2].style.display = 'none'
+        }
+        if(child_count == 1){
+            children_age[1].style.display = 'flex'
+            children_age[2].style.display = 'none'
+        }
+        if(child_count == 2){
+            children_age[1].style.display = 'flex'
+            children_age[2].style.display = 'flex'
+        }
+        
+    })
+})
+num_minus_room.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        if(room_count > 1){
+            room_count--
+            this.nextElementSibling.innerHTML = room_count
+            room_num[1].innerHTML = room_count
+        }
+    })
+})
+// 아동 나이 선택
+console.log(user_children_age,children_age_select)
+user_children_age.forEach((t,i)=>{
+    t.addEventListener('click',()=>{
+        children_age_select[i].classList.toggle('toggle')
+        down[i].classList.toggle('rotate')
+    })
+})
+
+// let select_span = 0
+// children_age_select_span.addEventListener('click',()=>{
+//     children_age_select_span.value = select_view[0].innerHTML
+// })
+
+// 다른곳 클릭하면 비활성화(인원체크)
+document.addEventListener('click', function(event) {
+    if (!numView.contains(event.target)) {
+        userSelect.style.display = 'none'
+    }
+});
+
+numView.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+userSelect.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
 
 //mobile 변수
 const search_box = document.querySelector('.search_box_m');
@@ -259,65 +390,3 @@ more_btn.addEventListener('click',()=>{
     sub_menu_list.classList.toggle('active')
     i_rotate.classList.toggle('rotate');
 })
-
-
-// 객실 인원 증가 감소 
-// const person_wrap = document.querySelectorAll('.person_wrap')
-const num_minus_room = document.querySelectorAll('.user_room_select .num_minus')
-const num_minus_adult = document.querySelectorAll('.user_adult_select .num_minus')
-const num_minus_child = document.querySelectorAll('.user_children_select .num_minus')
-const num_plus_room = document.querySelectorAll('.user_room_select .num_plus')
-const num_plus_adult = document.querySelectorAll('.user_adult_select .num_plus')
-const num_plus_child = document.querySelectorAll('.user_children_select .num_plus')
-const children_age = document.querySelector('.children_age')
-let room_count = 1
-let adult_count = 2
-let child_count = 0
-
-//성인, 아이, 객실 증가용 
-num_plus_adult.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        adult_count++
-        this.previousElementSibling.innerHTML = adult_count
-    })
-})
-num_plus_child.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        child_count++
-        this.previousElementSibling.innerHTML = child_count
-        // children_age.style.display = 'block'
-    })
-})
-num_plus_room.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        room_count++
-        this.previousElementSibling.innerHTML = room_count
-    })
-})
-//성인, 아이, 객실 감소용
-num_minus_adult.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        if(adult_count > 1){
-            adult_count--
-            this.nextElementSibling.innerHTML = adult_count
-        }
-    })
-})
-num_minus_child.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        if(child_count > 0){
-            child_count--
-            this.nextElementSibling.innerHTML = child_count
-        }
-    })
-})
-num_minus_room.forEach(function(t,i){
-    t.addEventListener('click',function(){
-        if(room_count > 1){
-            room_count--
-            this.nextElementSibling.innerHTML = room_count
-        }
-    })
-})
-
-
